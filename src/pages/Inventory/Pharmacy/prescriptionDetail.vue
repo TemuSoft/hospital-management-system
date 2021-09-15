66cf3a5b13907b989498e6d8fd6f6fbd409b2997
TemuSoft <temusoft2012>
2021-09-15 10:53:28 +0300

    Prescription detail store done

---
+++ src/pages/Inventory/Pharmacy/prescriptionDetail.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main"></div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+
+  created() {
+    const { prescriptionId } = this.$route.params;
+    this.prescriptionId = prescriptionId;
+
+    if (prescriptionId === undefined) this.back();
+
+    this.loadData();
+  },
+
+  computed: {
+    ...mapState("prescription", ["singlePrescription"]),
+  },
+
+  methods: {
+    ...mapActions("prescription", ["getSinglePrescription"]),
+
+    async loadData() {
+      await this.getSinglePrescription(this.prescriptionId);
+    },
+
+    async back() {
+      this.$router.push({ name: "prescription" });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  margin: 5%;
+  margin-top: 2%;
+}
+</style>
